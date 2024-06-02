706b06789853763bb311cd6df0a6217f6c204f65
Franck Fongang <162392919+Francklin9999@users.noreply.github.com>
2024-06-01 23:26:55 -0400

    Update Main.java

--- Sorting_Project_Java/Main.java
+++ Sorting_Project_Java/Main.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
 
 public class Main {
-    JLabel lblTtl;
-    Base app;
-    BubbleSort bubble;
-    public Main(){
-    }
     public static void main(String[] args){
         new Base();
     }
 }
+
